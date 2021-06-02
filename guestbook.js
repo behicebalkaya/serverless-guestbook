89d3058c440d40b1365ed74c940e928adaff4309
behicebalkaya <85236012+behicebalkaya@users.noreply.github.com>
2021-06-02 17:36:23 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4a34b7e3.eu-gb.apigw.appdomain.cloud/guestbook/entries';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
